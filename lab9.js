629a067b25879c3b3f3e5b7ab5562e6cf4ecc078
jromani148 <jr863@njit.edu>
2024-11-22 09:36:40 -0500

    Lab done

---
+++ lab9.js
@@ -0,0 +1,53 @@
+//question1
+let charaters = [`Harry`,`Hermione`,`Ron`,`Luna`];
+console.log(charaters);
+//question2
+let firstname = `Harry`;
+let house= `Gryffindor`;
+console.log(`welcome , ${firstname} ${house}`);
+//question3
+let characterName = `Hermione`;
+console.log(characterName.toUpperCase);
+console.log(characterName.toLowerCase);
+//question4
+let  spell = `  Expelliarmus     `;
+console.log(spell.trim);
+//question5
+let quote =`I solemnly swear that I am up to no good`;
+console.log(quote.slice(10,16));
+//question6
+let firsName = `Ron`;
+let lastName = `Weasley`;
+console.log(firsName.concat(lastName));
+//question7
+let sentence = `Draco is a good wizard`
+console.log(sentence.replace("good", "bad"))
+//question8
+let houses= ['Dumbledore', 'McGonagall', 'Hagrid'];
+houses.push(`slythering`);
+console.log(houses);
+
+//question9
+let spells = ['Alohomora', 'Lumos', 'Nox'];
+spells.unshift(`Accio`);
+console.log(spells);
+//question10
+let professors = ['Dumbledore', 'McGonagall', 'Snape', 'Hagrid'];
+console.log(professors.slice(1,3));
+//question11
+let students = ['Neville', 'Seamus', 'Dean', 'Parvati'];
+students.splice(1,2);
+console.log(students);
+//question12
+let phrase=`Mischief Managed`;
+phrase.toLowerCase.trim;
+console.log(phrase , `Harry`);
+//question13
+let wizards = ['Harry', 'Hermione', 'Ron']; 
+wizards.push (`luna`, `Draco`);
+console.log(wizards);
+//question14
+let message = 'Welcome to Hogwarts School of Witchcraft and Wizardry'
+console.log(message.slice(11,19).concat(" ","Caslte"));
+
+
